5e346e057195f662fff3a08456e501d518aeea35
Rajshr <Rajshr@users.noreply.github.com>
2019-08-19 14:44:58 +0530

    Update Scala.scala

--- src/main/scala/com/stratio/sonar/scalastyle/core/Scala.scala
+++ src/main/scala/com/stratio/sonar/scalastyle/core/Scala.scala
@@ -16,6 +16,7 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
+//Test push
 package com.stratio.sonar.scalastyle.core
 
 import com.stratio.sonar.scalastyle.Constants
